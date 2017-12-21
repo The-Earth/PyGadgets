91366844f80e15d8b369d7cdcc6eeea47f022938
The-Earth <tigerzeng97@gmail.com>
2017-12-21 18:07:10 +0800

    merge from QQClean & Single-side

---
+++ QQClean/Clean.py
@@ -0,0 +1,30 @@
+import os
+
+cwd = '/storage/emulated/0/tencent/'
+os.chdir(cwd)
+
+def listdr(directory):
+    rst = []
+    for dirs in os.listdir(directory):
+        if os.path.isdir(dirs):
+            rst.extend(listdr(directory+'/'+dirs))
+        rst.append(directory+'/'+dirs)
+    return set(rst)
+
+def cldir(directory):
+    for d in listdr(directory):
+        if 'QQ' in d :
+            if os.path.isdir(d):		
+                try:
+                    os.rmdir(d)
+                    print(d + ' removed.')
+                    continue
+                except OSError:
+                    cldir(d)
+
+            else:
+                os.remove(d)
+                print(d+' removed.')
+
+cldir(cwd)
+print('Done!')
---
+++ QQClean/README.md
@@ -0,0 +1,9 @@
+# QQClean
+For deleting all QQ-related files and directories after uninstalling QQ on Android.
+I write this tool only for fun.
+## Dependency
+[QPython](https://play.google.com/store/apps/details?id=org.qpython.qpy) or such.
+## What do I do.
+Delete all files and directories included in the given directories with "QQ" in their name.
+## How to use
+Put Clean.py under target directory (usually 'tencent') and run it. 

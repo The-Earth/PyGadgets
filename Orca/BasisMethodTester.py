fe64cb3f2ef33981f9556376b202d5e2b4d8ad58
The-Earth <tigerzeng97@gmail.com>
2018-03-26 10:24:35 +0800

    fixed typo

--- Orca/BasisMethodTester.py
+++ Orca/BasisMethodTester.py
@@ -8,12 +8,12 @@ def main(func, basis, inp)
 
     for i in func:
         for j in basis:
-            with open(#r'(directory for calculation input) ./calc/xxx.inp' ,'w') as inp:
+            with open(#r'(directory for calculation input)' ,'w') as inp:
                 inplist = modlist
-                inplist[n] = '! %s %s\n' % (i,j)    #replace n with the number blanked line -1
+                inplist[n] = '! %s %s\n' % (i,j)    #replace n with the number of blanked line
                 inp.writelines(inplist)
-            system(#r'(orca path) ./calc/xxx.inp > ./calc/xxx.out')
-            copy(#r'./calc/xxx.out', r'./xxx.out')
+            #system(r'(orca path) ./calc/xxx_%s_%s.inp > ./calc/xxx_%s_%s.out' %(i,j,i,j))
+            #copy(#r'./calc/xxx_%s%s.out'%(i,j), r'./xxx_%s_%s.out'%(i,j))
 
 if __name__ == '__main__':
     func = tuple(input('Functions to be tested (seperated by comma): ').split(','))

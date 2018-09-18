bfde952dc87e167fee145ed8b8436e3aacf4b1c8
The-Earth <tigerzeng97@gmail.com>
2018-09-18 11:08:35 +0800

    Reconstruct: Orca\BasisMethodTester

--- Orca/BasisMethodTester.py
+++ Orca/BasisMethodTester.py
@@ -1,22 +1,32 @@
 ﻿from os import system
 from shutil import copy
 
-def main(func, basis, inp)
 
-    with open(inp,'r') as mod:
-        modlist = mod.readlines()
+def main(basisSet, methodSet, orcaPath, rootName, modelFile, calcPath, blankLine):
+    modelTextList = list(open('%s%s'%(rootName, modelFile),'r'))
 
-    for i in func:
-        for j in basis:
-            with open(#r'(directory for calculation input)' ,'w') as inp:
-                inplist = modlist
-                inplist[n] = '! %s %s\n' % (i,j)    #replace n with the number of blanked line
-                inp.writelines(inplist)
-            #system(r'(orca path) ./calc/xxx_%s_%s.inp > ./calc/xxx_%s_%s.out' %(i,j,i,j))
-            #copy(#r'./calc/xxx_%s%s.out'%(i,j), r'./xxx_%s_%s.out'%(i,j))
+    if modelTextList[blankLine-1] != '\n':
+        raise Exception('检查：指定的空行不为空')
+
+    for i in basisSet:
+        for j in methodSet:
+            modelTextList[blankLine-1] = '! %s %s\n'%(i, j)
+            calcName = '%s%s%s_%s_%s.inp'%(rootName, calcPath, modelFile, i, j)
+            with open(calcName, 'w') as calcInp:
+                calcInp.write(''.join(modelTextList))
+
+            system('%s %s > %s'%(orcaPath, calcName, calcName.replace('.inp', '.out')))
+            copy(calcName.replace('.inp', '.out'), rootName)
+
 
 if __name__ == '__main__':
-    func = tuple(input('Functions to be tested (seperated by comma): ').split(','))
-    basis = tuple(input('Basis sets to be tested (seperated by comma): ').split(','))
-    inp = input('Model file name: ')
-    main(func, basis, inp)
+    basisSet = tuple(input('Basis Set (Separate each one with comma(,)): ').split(','))
+    methodSet = tuple(input('Method (Separate each one with comma(,)): ').split(','))
+    orcaPath = input('Directory of ORCA executable file (e.g. C:\orca\orca.exe): ')
+    rootName = input('Directory of the inp file as a model: ')
+    modelFile = input('Name of the inp file as a model: ')
+    calcPath = input('Directory to be used for calculation: (Should be started AND ended with \ or /): ')
+    blankLine = int(input('Line NO. of a blank line ready for insert basis and method: '))
+
+    main(basisSet=basisSet, methodSet=methodSet, orcaPath=orcaPath, modelFile=modelFile, calcPath=calcPath, \
+         blankLine=blankLine, rootName=rootName)

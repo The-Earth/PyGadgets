619d1229d4916fead313388b9ceedeeafdce20ea
The-Earth <tigerzeng97@gmail.com>
2019-01-19 10:01:14 +0800

    [Add]: submit [Add]: code comment

--- VASP/utest.py
+++ VASP/utest.py
@@ -4,16 +4,21 @@ import os
 key = input('Key: ')
 values = input('Values (separate by comma ): ').split(',')
 
-model = INCAR()
+model = INCAR() # read model file
 model_text = model.text_list()
 
-for i in range(len(values)):
+for i in range(len(values)):    # edit INCAR
     os.makedirs('%s%s' % (key, values[i]))
     future_file = '%s%s/INCAR' % (key, values)
     with open(future_file, 'w') as f:
         f.write('')
-    inob = INCAR()
+    inob = INCAR(filename=future_file)
     inob.text_list = model_text
     inob.set_key(key, values[i])
     inob.save()
     print(future_file, 'created.')
+
+for d in os.listdir():  # submit
+    if os.path.isdir(d):
+        if 'INCAR' in os.listdir(d):
+            os.system('bsub < vasp.sh')

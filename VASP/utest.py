596dcfb674e34a1c70a9929ebbd79e64be65fcc3
The-Earth <tigerzeng97@gmail.com>
2019-01-26 22:42:53 +0800

    [Modify]: Input and directory format.

--- VASP/utest.py
+++ VASP/utest.py
@@ -3,15 +3,16 @@ import os
 import shutil
 
 key = input('Key: ')
-values = input('Values (separate by comma ): ').split(',')
+values = input('Values (separate by ♂): ').split('♂')
 
 model = INCAR() # read model file
 model_text = model.text_list
 
 for i in range(len(values)):    # edit INCAR
-    if not os.path.exists('%s%s' % (key, values[i])):
-        os.makedirs('%s%s' % (key, values[i]))
-    future_file = '%s%s/INCAR' % (key, values[i])
+    path = '%s_%s' % (key, values[i].replace(',', '_'))
+    if not os.path.exists(path):
+        os.makedirs(path=path)
+    future_file = '%s/INCAR' % path
     with open(future_file, 'w') as f:
         f.write('')
     inob = INCAR(filename=future_file)
@@ -21,23 +22,23 @@ for i in range(len(values)):    # edit INCAR
     print(future_file, 'created.')
 
 for d in os.listdir():  # copy file
-    if os.path.isfile(d, ):
+    if os.path.isfile(d):
         for i in values:
             if d == 'vasp.sh':
                 with open(d, 'r') as f:
                     sub = f.read().replace('-J VASP', '-J VASP%s%s' % (key, i))
                 with open(d+'_temp', 'w') as f:
                     f.write(sub)
-                shutil.copy(d+'_temp', '%s%s/%s'%(key, i, d))
+                shutil.copy(d+'_temp', '%s_%s/%s'%(key, i, d))
                 os.remove(d+'_temp')
             elif d.endswith('.py') or d == 'INCAR':
                 pass
             else:
-                shutil.copy(d, '%s%s/%s' % (key, i, d))
+                shutil.copy(d, '%s_%s/%s' % (key, i, d))
 
-            print('%s%s/%s' % (key, i, d), 'copied.')
+            print('%s_%s/%s' % (key, i, d), 'copied.')
 
 for d in os.listdir(): # submit
     if os.path.isdir(d) and 'INCAR' in os.listdir(d):
-        os.system('bsub < %s/vasp.sh'%d)
+        os.system('bsub < %s/vasp.sh' % d)
         print(d, 'submitted.')

c7505dc0261def8442525f3bbe98e4b9869ca7cd
The-Earth <tigerzeng97@gmail.com>
2019-01-27 11:34:50 +0800

    [Add]: Submit only tool

---
+++ VASP/msubmit.py
@@ -0,0 +1,8 @@
+import os
+
+for d in os.listdir(): # submit
+    if os.path.isdir(d) and 'INCAR' in os.listdir(d):
+        os.chdir(d)
+        os.system('bsub < vasp.sh')
+        print(d, 'submitted.')
+        os.chdir('..')
